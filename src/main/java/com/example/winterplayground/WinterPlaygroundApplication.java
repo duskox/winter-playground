8cda4c8bcdaec2596ff3bbc04e361e8f311e0851
Dusan Vorkapic <dusan.vorkapic@futurice.com>
2021-11-28 21:35:31 +0100

    Initial commit, code generated from start.spring.io

---
+++ src/main/java/com/example/winterplayground/WinterPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.winterplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinterPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/winterplayground/WinterPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.winterplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
